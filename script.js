892650c44b87cd0d6b972d06d7dcd4c9ccc86de0
Your Name <24b01a42d2@svecw.edu.in>
2026-05-01 23:02:02 +0530

    updated js file

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("Portfolio Loaded Successfully");
+// Typing effect
+const text = "AIML Student | Full Stack Developer";
+let i = 0;
+
+function typeEffect() {
+    if (i < text.length) {
+        document.getElementById("typing").innerHTML += text.charAt(i);
+        i++;
+        setTimeout(typeEffect, 50);
+    }
+}
+
+document.getElementById("typing").innerHTML = "";
+typeEffect();
+
+
+// Form alert
+const form = document.querySelector("form");
+
+if (form) {
+    form.addEventListener("submit", function() {
+        alert("Message submitted successfully!");
+    });
+}
